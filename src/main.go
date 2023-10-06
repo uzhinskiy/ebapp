d3909fd0ece1ca430bd02111622e951a49b6af26
Boris <boris.uzhinsky@flant.com>
2023-10-06 11:21:59 +0300

    rand string

--- src/main.go
+++ src/main.go
@@ -4,14 +4,32 @@ import (
 	"os"
 	"strconv"
 	"time"
-
+	"math/rand"
+	"strings"
 	"github.com/rs/zerolog/log"
 )
+
+// define the given charset, char only
+var charset = []byte("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+var number = []byte("0123456789")
+var alphaNumeric = append(charset, number...)
+
+// n is the length of random string we want to generate
+func randStr(n int) string {
+    b := make([]byte, n)
+    for i := range b {
+	// randomly select 1 character from given charset
+	b[i] = alphaNumeric[rand.Intn(len(alphaNumeric))]
+    }
+    return string(b)
+}
 
 func main() {
 	var i int
 	var sleep int
 	var service string
+
+	rand.Seed(time.Now().UnixNano())
 
 	tmp1, e1 := os.LookupEnv("SERVICE")
 	if e1 {
@@ -26,10 +44,10 @@ func main() {
 	} else {
 		sleep = 10
 	}
-
+        msg := strings.Repeat(randStr(1024), 10)
 	for {
 		i++
-		log.Info().Str("service", service).Int("iterator", i).Msg("Hello")
+		log.Info().Str("service", service).Int("iterator", i).Msg(msg)
 		time.Sleep(time.Millisecond * time.Duration(500*sleep))
 	}
 
